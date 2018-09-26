48e5bf3a81947ef10deabed95ed41d955de2b400
leonardomso <leonardomso11@gmail.com>
2018-09-26 12:49:04 -0300

    remove lock json

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === "production") {
   // Set static folder.
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
