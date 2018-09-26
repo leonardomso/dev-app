265610d2ccdd2b76a8c57b75b8b4f782e034953c
leonardomso <leonardomso11@gmail.com>
2018-09-26 12:42:50 -0300

    removed logger

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 
 import rootReducer from "../reducers/";
 
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk);
 
 const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
